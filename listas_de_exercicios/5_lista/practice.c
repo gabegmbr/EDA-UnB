b4ba73cd822015ce581f817764c6603ac8c9fe81
gabegmbr <221022560@aluno.unb.br>
2024-05-04 14:12:38 -0300

    something something

--- listas_de_exercicios/5_lista/practice.c
+++ listas_de_exercicios/5_lista/practice.c
@@ -6,14 +6,39 @@ int *dados;
 int N, topo;
 } pilha;
 
-int empilha (pilha *p, int x);
+int redimensiona(pilha *p){
+
+}
+
+int empilha (pilha *p, int x){
+    if(p->topo == p->N){
+        return 0;
+    }
+    p->dados[p->topo++] = x;
+}
+
+int desempilha(pilha *p){
+    if(p->topo-1 == -1){
+        p->dados[p->topo]
+    }else{
+        p->dados[p->topo--];
+    }
+}
 
 int main (){
     pilha *stack = malloc(sizeof(stack));
     stack->N = 5;
+    stack->topo = 0;
     stack->dados = (int*)malloc(stack->N * sizeof(int));
     if(stack->dados == NULL){
         printf("memory allocation error.\n");
         exit(0);
+    }
+
+    empilha(stack, 1);
+    empilha(stack, 2);
+
+    for(int i = 0; i < stack->topo; i++){
+        printf("%d ", stack->dados[i]);
     }
 }
