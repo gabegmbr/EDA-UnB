0053690eca785e20e732c6ec158e769442ebbd3f
gabegmbr <221022560@aluno.unb.br>
2024-05-05 17:16:04 -0300

    implementação de fila encadeada concluída

---
+++ listas_de_exercicios/5_lista/fila_encadeada.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct celula {
+    int dado;
+    struct celula *prox;
+} celula;
+
+void *enfileira (celula **f, int x){
+    celula *novo = malloc(sizeof(celula));
+    if(novo == NULL) return NULL;
+    novo->prox = (*f)->prox;
+    (*f)->prox = novo;
+    (*f)->dado = x;
+    *f = novo;
+}
+
+int desenfileira (celula *f, int *y){
+    if(f->prox == f){
+        return 0;
+    }
+    celula *remove = f->prox;
+    *y = remove->dado;
+    f->prox = remove->prox;
+    free(remove);
+    return 1;
+}
