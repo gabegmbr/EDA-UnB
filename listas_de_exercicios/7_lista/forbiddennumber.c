30d34b2b525124292d2118c01597288d383860c9
gabegmbr <221022560@aluno.unb.br>
2024-06-24 18:38:26 -0300

    numero proibido

--- listas_de_exercicios/7_lista/forbiddennumber.c
+++ listas_de_exercicios/7_lista/forbiddennumber.c
@@ -49,10 +49,9 @@ int partition(int *arr, int l, int r){
     return j+1;
 }
 
-
 void quickSort(int *arr, int l, int r){
     if (l <= r){
-        if(r-l <= 100) return selection_sort(arr, l, r+1);
+        if(r-l <= 1000) return selection_sort(arr, l, r+1);
         mediana(arr, l, r);
         int pi = partition(arr, l, r);
         quickSort(arr, l, pi-1);
@@ -60,12 +59,30 @@ void quickSort(int *arr, int l, int r){
     }
 }
 
-int main (){
+int binary_search(int v[], int n, int num){
+    int inicio = 0;
+    int fim = n-1;
+    int meio;
+    while(inicio <= fim){
+        meio = inicio + (fim - inicio) / 2;
+        if(v[meio] == num) return meio;
+        if(num > v[meio]) inicio = meio + 1;
+        else fim = meio - 1;
+    }
+    return -1;
+}
+
+int main () {
     int n;
     scanf("%d", &n);
-    int *arr = malloc(sizeof(int)*n);
+    int *numeros = malloc(sizeof(int)*n);
+    for(int i = 0; i < n; i++) scanf("%d", &numeros[i]);
+    quickSort(numeros, 0, n-1);
 
-    for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
-    quickSort(arr, 0, n-1);
-    for(int i = 0; i < n; i++) printf("%d ", arr[i]);
+    int numero;
+    while(scanf("%d", &numero) == 1){
+        int resultado = binary_search(numeros, n, numero);
+        if(resultado == -1) printf("nao\n");
+        else printf("sim\n");
+    }
 }
